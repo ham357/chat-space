bd6c39ecc59f48f1473483dbc378c5106723c971
ham357 <sakuraniumarete@gmail.com>
2019-03-11 11:47:23 +0900

    Fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Rails.application.routes.draw do
   root    'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:create, :new, :edit, :update] do
-<<<<<<< HEAD
     resources :messages, only: [:index,:create]
-=======
-    resources :messages, only: [:index,:  create]
->>>>>>> parent of 21aac9c... Revert "Merge pull request #9 from ham357/Create-Message-Model"
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
