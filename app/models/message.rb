b0187703f8362e1a1da24e2f46b92c57f34e4e6d
ham357 <sakuraniumarete@gmail.com>
2019-03-11 11:45:37 +0900

    Revert Revert Create-Message-Model

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,8 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, unless: :image?
+<<<<<<< HEAD
   mount_uploader :image, ImageUploader
+=======
+>>>>>>> parent of 21aac9c... Revert "Merge pull request #9 from ham357/Create-Message-Model"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root    'groups#index'
-  resources :users, only: [:index, :edit, :update]
+  resources :users, only: [:edit, :update]
   resources :groups, only: [:create, :new, :edit, :update] do
+<<<<<<< HEAD
     resources :messages, only: [:index,:create]
+=======
+    resources :messages, only: [:index,:  create]
+>>>>>>> parent of 21aac9c... Revert "Merge pull request #9 from ham357/Create-Message-Model"
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
