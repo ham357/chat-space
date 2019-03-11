a5090eafff6cd129b9ed12c301d72db46e512ff4
ham357 <sakuraniumarete@gmail.com>
2019-03-11 11:46:21 +0900

    Fix message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,8 +3,5 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, unless: :image?
-<<<<<<< HEAD
   mount_uploader :image, ImageUploader
-=======
->>>>>>> parent of 21aac9c... Revert "Merge pull request #9 from ham357/Create-Message-Model"
 end
