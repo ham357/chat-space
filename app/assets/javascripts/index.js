4f3e5981f32c731f0641276f087ca26b0ec69def
ham357 <sakuraniumarete@gmail.com>
2019-03-19 12:27:01 +0900

    Add function appendUser

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,13 @@
 $(function(){
-  // var search_list = $("#user-search-result");
+  var search_list = $("#user-search-result");
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+    <p class="chat-group-user__name">${ user.name }</p>
+    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+  </div>`
+    
+      search_list.append(html);
+   }
 
     $('#user-search-field').on("keyup", function () {
       var input = $("#user-search-field").val();
@@ -20,11 +28,11 @@ $(function(){
           });
         }
         else{
-          // appendErrMsgToHTML("一致するユーザーはいません");
+          appendErrMsgToHTML("一致するユーザーはいません");
         }
       })
       .fail(function(){
-        // alert('ユーザー検索に失敗しました');
+        alert('ユーザー検索に失敗しました');
       })
     });
   });
