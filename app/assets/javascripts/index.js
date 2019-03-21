0e475845769cc46d6186e704528fea15972f1400
ham357 <sakuraniumarete@gmail.com>
2019-03-21 15:17:29 +0900

    Refactoring code

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,6 @@
 $(function(){
-  var search_list = $("#user-search-result");
+  var search_result = $("#user-search-result");
+  var textField = $("#user-search-field");
   var preWord = "";
 
   function appendUser(user) {
@@ -8,7 +9,7 @@ $(function(){
     <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
   </div>`
     
-      search_list.append(html);
+    search_result.append(html);
    }
 
    function appendErrMsgToHTML(msg) {
@@ -16,17 +17,20 @@ $(function(){
     <p class="chat-group-user__name">${ msg }</p>
     </div>`
     
-    search_list.append(html);
+    search_result.append(html);
   }
 
    $(document).on("click", ".chat-group-user__btn--add", function () {
+    var add_chat_member_element = $(".js-chat-member").parent();
+    var user_id = $(this).attr("data-user-id");
+    var user_name = $(this).attr("data-user-name");
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-    <input name='group[user_ids][]' type='hidden' value='${ $(this).attr("data-user-id")}'>
-    <p class='chat-group-user__name'>${ $(this).attr("data-user-name") }</p>
+    <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+    <p class='chat-group-user__name'>${ user_name }</p>
     <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
   </div>`
 
-    $("#add-chat-member").append(html);
+    add_chat_member_element.append(html);
     $(this).parent().remove();
 
    });
@@ -37,8 +41,8 @@ $(function(){
 
   });
 
-    $('#user-search-field').on("keyup", function () {
-      var input = $("#user-search-field").val();
+  textField.on("keyup", function () {
+      var input = textField.val();
       var Word = input;
       
       if (Word !== preWord){
@@ -50,14 +54,14 @@ $(function(){
         })
     
         .done(function(users){
-          $("#user-search-result").empty();
-          var search_target = $('.chat-group-user__name').text();
+          search_result.empty();
+          var group_members = $('.chat-group-user__name').text();
           if (users.length !== 0){
             if(input !== ""){
               users.forEach(function(user,index){
-                if (search_target.indexOf(user.name) == -1){
+                if (group_members.indexOf(user.name) == -1){
                   appendUser(user);
-                }else if(index == users.length - 1 && $("#user-search-result").text() == ""){
+                }else if(index == users.length - 1 && search_result.text() == ""){
                   appendErrMsgToHTML("一致するユーザーはいません");
                 }
               });
