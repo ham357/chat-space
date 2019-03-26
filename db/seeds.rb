eeb793005eacbc9e07b0cded4c070567e5956993
ham357 <sakuraniumarete@gmail.com>
2019-03-26 11:34:19 +0900

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  User.create(
+  name = Faker::JapaneseMedia::DragonBall.character
+  email = Faker::Internet.email
+  password = 11111111
+  password_confirmation = 11111111
+  )
+end
