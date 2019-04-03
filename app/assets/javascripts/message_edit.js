7b338d4e648afab27a5aaf965ae54ff1c7d840ba
ham357 <sakuraniumarete@gmail.com>
2019-04-03 13:05:05 +0900

    編集完了ボタンとキャンセルボタンの配置

--- app/assets/javascripts/message_edit.js
+++ app/assets/javascripts/message_edit.js
@@ -1,8 +1,29 @@
 $(function() {
   $('.message__upper-info__edit-message').on('click', function(e) {
-    e.preventDefault();
-    console.log("hello");
-   
-  });
+    // var message_element = $(this).parents('.message');
+    // var message_id = message_element.attr("data-id");
+    // var url = location.href + "/" + message_id;
+    // console.log(url);   
+    var InlineEdit = $(this).parents('.message');
+    var input_element = '<input class="message__InlineEdit-input" type="text" value="">';
+      var button_element = '<ul class="message__InlineEdit-buttons"><li class="message__InlineEdit-cancel"><i class="fa  fa-times"></i></li><li class="message__InlineEdit-save"><i class="fa fa-check-circle"></i></li></ul>'; //Save/Cancel floating buttons
+      InlineEdit.addClass('InlineEdit-active').empty().append(input_element).append(button_element).find('input').focus();
 
+
+    // $.ajax({
+    //   url: url,
+    //   type: "POST",
+    //   data: {'id': message_id,'content': content,
+    //   '_method': 'patch'} ,
+    //   dataType: 'json'
+    // })
+    // .done(function(data) {
+    //   message_element.remove();
+    // })
+    
+    // .fail(function() {
+    //   alert('message destroy error');
+    // })
+  // }
+  });
 });
--- app/assets/javascripts/mouseover_out_btn.js
+++ app/assets/javascripts/mouseover_out_btn.js
@@ -1,12 +1,12 @@
 $(function() {
   var message_btn;
   $('.message').mouseover(function() {
-    message_btn = $(this).find('ul')
+    message_btn = $(this).find('.message__upper-info__menu')
     message_btn.show();
     message_btn.css("display", "flex")
   })
   $('.message').mouseout(function() {
-    message_btn = $(this).find('ul')
+    message_btn = $(this).find('.message__upper-info__menu')
     message_btn.hide();
   })
 });
