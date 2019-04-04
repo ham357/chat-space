c94001d786212ad925af91a9353c69426c180fec
ham357 <sakuraniumarete@gmail.com>
2019-04-04 11:30:12 +0900

    mouse hoverの修正
    メッセージ編集と削除ボタンの非同期化による修正
    自分の投稿にのみ表示するように修正
    ファイル名も内容がわかりやすいものに修正

---
+++ app/assets/javascripts/mouseover_hover_action.js
@@ -0,0 +1,24 @@
+$(function() {
+  var message_element;
+  var message_menu_element;
+  var current_user_id = $('.side-header__box__user-name').attr("data-current_user_id");
+
+  function message_btn_build() {
+    var html = '<ul class="message__upper-info__menu"><li class="message__upper-info__edit-message"><i class="fa fa-pencil-square-o"></i></li><li class="message__upper-info__destroy-message"><i class="fa fa-times"></i></li></ul>';
+    message_element.append(html);
+  }  
+  $(document).on({
+    "mouseenter": function(){
+    var message_user_id = $(this).attr("data-user_id");
+    console.log(message_user_id);
+    if (current_user_id == message_user_id){
+      message_element = $(this).find('.message__upper-info');
+      message_btn_build();
+    }},
+    "mouseleave": function(){
+      message_menu_element = $(this).find('.message__upper-info__menu');
+      message_menu_element.remove();
+    }
+    }, ".message");
+
+});
